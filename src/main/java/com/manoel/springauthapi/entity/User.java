7bc4ad6f5e868f64ab07ace92a8ccf02c911ccc3
manoel-alves <manoelann@hotmail.com>
2025-11-26 17:35:33 -0300

    feat: create user entity

---
+++ src/main/java/com/manoel/springauthapi/entity/User.java
@@ -0,0 +1,26 @@
+package com.manoel.springauthapi.entity;
+
+import com.manoel.springauthapi.enums.Role;
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+@Entity
+@Table(name = "users")
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(unique = true, nullable = false)
+    private String email;
+
+    @Column(nullable = false)
+    private String password;
+
+    @Enumerated(EnumType.STRING)
+    @Column(nullable = false)
+    private Role role;
+}
---
+++ src/main/java/com/manoel/springauthapi/enums/Role.java
@@ -0,0 +1,6 @@
+package com.manoel.springauthapi.enums;
+
+public enum Role {
+    USER,
+    ADMIN
+}
